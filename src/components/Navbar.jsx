c8276c99e259b13fdf8fbbf1582a87f1da4ebe45
DEV-BR0 <lazizbekxoljigitov@gmail.com>
2026-09-15 18:45:27 +0500

    Update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,8 @@
+import { Search } from 'lucide-react'
 import Logo from '../assets/logo.png'
 
 export default function Navbar() {
+  const search = <Search />
   return (
     <nav className="w-full bg-[#00111A] flex text-white">
       <div className="w-full flex items-center gap-[30px] container p-[20px] bg-[#00111A">
@@ -11,11 +13,10 @@ export default function Navbar() {
         <div className="w-[50%]">
           <input
             type="text"
-            placeholder={``}
+            placeholder={`${search} noder`}
             className="bg-[#0D1D25] rounded-[7px] p-[10px] text-white outline-none w-full"
           />
         </div>
-        ckndscnjdsk
       </div>
     </nav>
   )
